09864f6e3b71795843c3f34f6415f434367836fb
ChristianLin0420 <crlc112358@gmail.com>
2022-03-01 23:18:22 +0800

    update problem 29-34 solutions

---
+++ medium/29_divide_two_integers.cpp
@@ -0,0 +1,32 @@
+
+#include<iostream>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        int sign = (dividend >= 0 ^ divisor >= 0) ? -1 : 1;
+        long long int quotient = 0;
+        long long int sum = 0;
+        
+        long long int dividendL = dividend;
+        dividendL = abs(dividendL);
+        long long int divisorL = divisor;
+        divisorL = abs(divisorL);
+        
+        for (int i = 31; i >= 0; i--) {
+            if (sum + (divisorL << i) <= dividendL) {
+                sum += (divisorL << i);
+                quotient += (1LL << i);
+            }
+        }
+        
+        if (sign * quotient > INT_MAX)
+            return INT_MAX;
+            
+        
+        return sign * quotient;
+    }
+};
---
+++ medium/36_valid_sudoku.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+    }
+};
