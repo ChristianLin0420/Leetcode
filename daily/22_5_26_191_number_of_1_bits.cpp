d89533b1dd74e2aaa6164d5b5e04fb7e6f2b5dda
ChristianLin0420 <crlc112358@gmail.com>
2022-05-28 23:06:19 +0800

    add new daily problems

---
+++ daily/22_5_26_191_number_of_1_bits.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        
+        while (n > 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        
+        return count;
+    }
+};
---
+++ daily/22_5_28_268_missing_number.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0, n = nums.size();
+        
+        for (auto &a : nums) {
+            sum += a;
+        }
+        
+        return 0.5 * n * (n + 1) - sum;
+    }
+};
